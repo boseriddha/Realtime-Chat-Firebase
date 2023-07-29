21fe8327e8516873979266ad89df1d59cabefcec
boseriddha <boseriddha@gmail.com>
2023-07-30 02:01:38 +0530

    setup layout and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <h1 className="text-3xl">NavBar</h1>
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Provider from "./components/Provider.tsx";
+import Dashboard from "./screens/Dashboard.tsx";
+import RegisterScreen from "./screens/RegisterScreen.tsx";
+import LoginScreen from "./screens/LoginScreen.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Dashboard />} />
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
