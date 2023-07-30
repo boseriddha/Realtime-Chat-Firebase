a2a915f1a9043b49283c741322abaac45cf87a46
boseriddha <boseriddha@gmail.com>
2023-07-30 16:15:43 +0530

    added db helpers

--- src/firebaseDb.ts
+++ src/firebaseDb.ts
@@ -1,4 +1,27 @@
-import { getFirestore } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  setDoc,
+  getFirestore,
+  getDoc,
+  DocumentData,
+} from "firebase/firestore";
 import { app } from "./firebaseConfig";
 
 export const db = getFirestore(app);
+
+// helpers
+
+// add data
+export const firestoreAddData = async (user: User): Promise<void> => {
+  const collectionRef = collection(db, "users");
+  await setDoc(doc(collectionRef, user.userId), user);
+};
+
+// get data
+export const firestoreGetData = async (
+  id: string
+): Promise<DocumentData | null> => {
+  const docRef = doc(db, "users", id);
+  return getDoc(docRef);
+};
