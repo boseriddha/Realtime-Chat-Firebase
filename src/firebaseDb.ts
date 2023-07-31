8a2e9a021eefd267bc07a6a1d3000c6ec4eb29f6
boseriddha <boseriddha@gmail.com>
2023-08-01 00:13:43 +0530

    fixed getAllFriends function

--- src/firebaseDb.ts
+++ src/firebaseDb.ts
@@ -5,9 +5,11 @@ import {
   getFirestore,
   getDoc,
   DocumentData,
+  updateDoc,
+  arrayUnion,
+  getDocs,
 } from "firebase/firestore";
 import { app } from "./firebaseConfig";
-
 export const db = getFirestore(app);
 
 // helpers
@@ -24,4 +26,87 @@ export const firestoreGetData = async (
 ): Promise<DocumentData | null> => {
   const docRef = doc(db, "users", id);
   return getDoc(docRef);
+};
+
+// update array
+export const firestoreAddFriend = async (id: string, idToAdd: string) => {
+  const collectionRef1 = doc(db, "users", id);
+  const collectionRef2 = doc(db, "users", idToAdd);
+  try {
+    await updateDoc(collectionRef1, {
+      friends: arrayUnion(idToAdd),
+    });
+
+    await updateDoc(collectionRef2, {
+      friends: arrayUnion(id),
+    });
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message);
+  }
+};
+
+// search and get friend
+export const firestoreSearchFriend = async (
+  email: string
+): Promise<SearchType> => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  const foundUser: SearchType = {
+    userId: "",
+    found: false,
+  };
+  querySnapshot.forEach((doc) => {
+    if (email === doc.data().email) {
+      foundUser.userId = doc.data().userId;
+      foundUser.found = true;
+    }
+  });
+  return foundUser;
+};
+
+// check friend
+export const firestoreCheckFriend = async (userId: string, idToAdd: string) => {
+  const docRef = doc(db, "users", userId);
+  const res = await getDoc(docRef);
+  let flag = 0;
+  if (res.exists()) {
+    const friends = res.data().friends;
+    friends.forEach((id: string) => {
+      if (idToAdd === id) {
+        flag = 1;
+      }
+    });
+  }
+
+  if (flag) return true;
+  else false;
+};
+
+// get all friends
+export const firestoreGetAllFriends = async (userId: string) => {
+  const response = await getDoc(doc(db, "users", userId));
+  let friendIdList: string[] = [];
+
+  if (response.exists()) {
+    friendIdList = response.data().friends;
+  }
+
+  let friendObjList: FriendType[] = [];
+
+  const promiseList = friendIdList.map(async (friendId) => {
+    const friendDoc = await getDoc(doc(db, "users", friendId));
+    return friendDoc;
+  });
+
+  const friendList = await Promise.all(promiseList);
+
+  friendList.forEach((friend) => {
+    if (friend.exists()) {
+      friendObjList.push({
+        friendName: friend.data().name,
+        friendId: friend.id,
+      });
+    }
+  });
+
+  return friendObjList;
 };
