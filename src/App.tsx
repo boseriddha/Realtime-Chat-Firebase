beb34be7df874405e98e8db796f5909c606402e7
boseriddha <boseriddha@gmail.com>
2023-08-01 00:13:17 +0530

    App commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl">NavBar</h1>
+    <div className="bg-white">
       <Outlet />
-    </>
+    </div>
   );
 }
 
